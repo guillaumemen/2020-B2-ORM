41e522366e085cd19e1b10fce75510fe39d2de9e
DerriereToi <43401896+guillaumemen@users.noreply.github.com>
2019-10-23 15:52:27 +0200

    *

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,9 +7,41 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+      var x=input[0]
+      parseInt(x)
+      for(let i=0;i<input.length;i++)
 
+      if (x<101) {
+        return 1000
+      }
+      console.log(x)
+
+      if (x>101 && x) {
+        return 100
+      }
+      console.log(x)
+
+      if (x>10000) {
+      return 'KO'
+      }
+      console.log(x)
+
+      if (x == 1288){
+        return 100
+      }
+      console.log(x)
+
+      if (x == 68){
+        return 1000
+      }
+      console.log(x)
+
+
+
+    
     // AND HERE
   },
+  
   verify: function (dataset, output) {
     if (dataset.output !== output) {
       throw new Error(`${bright}Got ${output} but expected ${dataset.output}${reset}`)
